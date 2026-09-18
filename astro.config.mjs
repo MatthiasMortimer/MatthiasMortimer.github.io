6e53ccf9a574bbca62901994ff76ddbf50abd964
Matthias Mortimer <matthiasdunlap007@gmail.com>
2026-09-17 23:04:11 -0500

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://matthiasmortimer.github.io',
+	site: 'https://github.io',
 	integrations: [mdx()]
 });
