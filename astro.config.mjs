e6ae14d7527281d40a2e9622040a68acf3b67bf2
Matthias Mortimer <matthiasdunlap007@gmail.com>
2026-09-18 00:49:36 -0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.io',
+	site: 'ritesdev.dev',
     base: '/',   // Explicitly set base to '/' so all assets resolve to the root domain root
-	integrations: [mdx()]
+	integrations: [mdx()],
+	// Change "static" to "server"
+  output: 'server', 
+  
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    // Ensure your platformProxy matches the bindings in your logs
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
