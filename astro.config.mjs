9457902ddde91afff72be2bb92bb538ab268a5fe
Matthias Mortimer <matthiasdunlap007@gmail.com>
2026-09-18 00:20:07 -0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.io',
+    base: '/',   // Explicitly set base to '/' so all assets resolve to the root domain root
 	integrations: [mdx()]
 });
